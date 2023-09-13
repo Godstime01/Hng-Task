592da1d0f653c63b7a8c800d9ec1123c152a76b5
godstime01 <youngestkid01@gmail.com>
2023-09-13 09:12:19 +0100

    second task

---
+++ stage two/next-template-jsx/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'api.themoviedb.org',
+            port: '',
+            pathname: '/3/**',
+          },
+        ],
+      },
+}
+
+module.exports = nextConfig
